6a42b3f7d19b73b5ba1ef1d1eae55d120b9106ef
yelgogogo <yelgogogo@hotmail.com>
2017-02-05 11:16:44 +0800

    add diskdb

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
     var bodyParser = require('body-parser');
     var multer = require('multer');
     var fs=require("fs");  
-    var fse = require('fs-extra')
+    var fse = require('fs-extra');
     var zlib = require('zlib'); 
     var gzip = zlib.createGzip(); 
     var fstream = require('fstream');
     var tar = require('tar');
-
+    var db = require('diskdb');
+    // var iconv = require('iconv-lite');
 
 
 
@@ -65,11 +66,11 @@
                 wsDB[f1] = req.body;
 
                 fs.writeFile(workspacesDB, JSON.stringify(wsDB), function(err){  
-                    if(err)  
+                    if(err){  
                         console.log("fail " + err);  
-                    else  
+                    }else{  
                         console.log("write file ok"); 
-                        res.json(wsDB);
+                        res.json(wsDB);}
                 });  
             }
         });    
@@ -82,7 +83,7 @@
             }else{ 
                 console.log(req.params); 
                 var wsDB = JSON.parse(data);
-                var f2 = wsDB.findIndex(function(element){return element.id == req.params.did});
+                var f2 = wsDB.findIndex(function(element){return element.id == req.params.did;});
                 var delworkspace = wsDB[f2]; 
                 wsDB.splice(f2, 1);
 
@@ -91,8 +92,8 @@
                         console.log("fail " + err);  
                     }else{ 
                         fse.remove(delworkspace.path, function (err) {
-                            if (err) return console.error(err)
-                            console.log('success!')
+                            if (err) return console.error(err);
+                            console.log('success!');
                         });
                         console.log("write file ok"); 
                         res.json(delworkspace);
@@ -120,11 +121,11 @@
                 wsDB.push( postw);
                 fse.ensureDirSync(postw.path);
                 fs.writeFile(workspacesDB, JSON.stringify(wsDB), function(err){  
-                    if(err)  
+                    if(err) {
                         console.log("fail " + err);  
-                    else  
+                    }else{  
                         console.log("write file ok"); 
-                        res.json(wsDB);
+                        res.json(wsDB);}
                 });  
             }
         });    
@@ -249,14 +250,17 @@
 
     var usersDB = 'db/usersdb.txt';
     app.get('/users', function(req, res){
-        fs.readFile(usersDB,'utf-8',function(err,data){  
-            if(err){  
-                console.log(err);     
-                // throw err;
-            }else{ 
-                res.send(data);        
-            }
-        }); 
+        db.connect('db', ['users']);
+        //console.log(db.users.find());
+        res.send(db.users.find());
+        // fs.readFile(usersDB,'utf-8',function(err,data){  
+        //     if(err){  
+        //         console.log(err);     
+        //         // throw err;
+        //     }else{ 
+        //         res.send(data);  
+        //     }
+        // }); 
     });
 
 
@@ -273,22 +277,25 @@
       var file= filer.path ;
       console.log(filer);
       var regArray = JSON.parse(req.query.regs);
-        fs.readFile(file,filer.encoding,function(err,data){  
+        fs.readFile(file,'binary',function(err,data){  
         if(err){  
             console.log("error");  
         }else{  
-            var delimiter='\n';
-            switch(filer.encoding){
-                case 'utf8':
-                case 'utf-8':
-                    delimiter='\n';
-                    break;
-                case 'utf-8':
-                    delimiter='\r\n';
-                    break;
-            }
+            // var delimiter='\n';
+            // switch(filer.encoding){
+            //     case 'utf8':
+            //     case 'utf-8':
+            //         delimiter='\n';
+            //         break;
+            //     case 'utf-8':
+            //         delimiter='\r\n';
+            //         break;
+            // }
             var out = data;
             var outf = data;
+            // var outff = iconv.decode(data, 'ascii');
+            // console.log('1--'+out);
+            // console.log('2--'+outff);
             // var filea = data.split(delimiter);
             //var regExp1 = /.*\r\n/g;
             // var regExp1 = new RegExp('.*'+delimiter,'g');
@@ -304,12 +311,12 @@
                 for (j=0; j < regArray.length; ++j) {
                     //console.log('-->'+regArray[j].enable); 
                     if (regArray[j].enable){
-                        if (regArray[j].regScope,regArray[j].regScopeAttr.value !== 'cg'){
+                        // if (regArray[j].regScope,regArray[j].regScopeAttr.value !== 'cg'){
                             var regExpScope = new RegExp(regArray[j].regScope,regArray[j].regScopeAttr.value);
                             var regExpScopeR = new RegExp(regArray[j].regScope,'');
                             regExpScope.lastIndex=0;
                             while ((result=regExpScope.exec(out)) !== null)  {
-                                var row=result.toString();
+                                var row=result[0].toString();
                                 for (k=0; k < regArray[j].findArray.length; ++k) {
                                     var regExpFind = new RegExp(regArray[j].findArray[k].regFind,regArray[j].findArray[k].regFindAttr.value);
                                     regExpFind.lastIndex=0;
@@ -327,25 +334,26 @@
                                 // console.log(regExpScopeR.lastIndex);
                                 outf = outf.replace(regExpScopeR,row);
                             }
-                        }else{
-                            regArray[j].regScopeAttr.value=regArray[j].regScopeAttr.value.replace(/c/,'')
-                            var regExpScope = new RegExp(regArray[j].regScope,regArray[j].regScopeAttr.value);
-                            console.log(regExpScope);
-                            result=regExpScope.exec(out);
-                            for (k=0; k < regArray[j].findArray.length; ++k) {
-                                    var regExpFind = new RegExp(regArray[j].findArray[k].regFind,regArray[j].findArray[k].regFindAttr.value);
-                                    regExpFind.lastIndex=0;
-                                    //console.log(regExpScope.exec(out));
-                                    //console.log('I:'+result.index+"|"+regExpFind + "F/R" +regArray[j].regReplace);
+                        //}
+                        // else{
+                        //     // regArray[j].regScopeAttr.value=regArray[j].regScopeAttr.value.replace(/c/,'')
+                        //     var regExpScope = new RegExp(regArray[j].regScope,regArray[j].regScopeAttr.value);
+                        //     console.log(regExpScope);
+                        //     result=regExpScope.exec(out);
+                        //     for (k=0; k < regArray[j].findArray.length; ++k) {
+                        //             var regExpFind = new RegExp(regArray[j].findArray[k].regFind,regArray[j].findArray[k].regFindAttr.value);
+                        //             regExpFind.lastIndex=0;
+                        //             //console.log(regExpScope.exec(out));
+                        //             //console.log('I:'+result.index+"|"+regExpFind + "F/R" +regArray[j].regReplace);
                                     
-                                    outf = outf.replace(regExpFind,regArray[j].findArray[k].regReplace);
+                        //             outf = outf.replace(regExpFind,regArray[j].findArray[k].regReplace);
                                     
-                                    //console.log('f-->'+filea[f]);
-                            }
-                            //console.log(result.toString());
-                            if(result)
-                                console.log(result.toString());
-                        }
+                        //             //console.log('f-->'+filea[f]);
+                        //     }
+                        //     //console.log(result.toString());
+                        //     if(result)
+                        //         console.log(result.toString());
+                        // }
                         //console.log(filea);
                     }
                 }
@@ -354,6 +362,7 @@
                 var fileOut = filer.convPath; //'./test/readme.txt'; 
                 //var arr=filea.join(delimiter);
                 var arr=outf;
+                var opt={encoding: 'binary'};
                 //console.log(arr)
                 fs.exists(fileOut,function(exists){  
                     if(exists){  
@@ -363,24 +372,26 @@
                                 //throw err;
                             }
                                 console.log('file:'+fileOut+' deleted');
-                                fs.appendFile(fileOut, arr, function(err){  
-                                    if(err)  
+
+                                fs.appendFile(fileOut, arr,opt, function(err){  
+                                    if(err) { 
                                         console.log("fail " + err);  
-                                    else  
-                                        console.log("write file ok");  
+                                    }else{  
+                                        console.log("write file ok");
+                                        filer.convFlag = true;
+                                        res.send(filer);}  
                                 });  
-                                filer.convFlag = true;
-                                res.send(filer);
+                                
                             }); 
                     }else{  
-                        fs.appendFile(fileOut, arr, function(err){  
-                                    if(err)  
+                        fs.appendFile(fileOut, arr,opt, function(err){  
+                                    if(err) { 
                                         console.log("fail " + err);  
-                                    else  
-                                        console.log("write file ok");  
-                                });  
-                                filer.convFlag = true;
-                                res.send(filer);
+                                    }else{  
+                                        console.log("write file ok");
+                                        filer.convFlag = true;
+                                        res.send(filer);}  
+                                }); 
                     }  
                 });  
             }  
@@ -392,8 +403,45 @@
     app.get('/:file(*)', function(req, res, next){
       var file = req.params.file;
       var path = __dirname + '/' + file;
-      console.log(path);
-      res.download(path);
+      var folder1 = file.match(/[^\\\/]+/);
+      switch (folder1.toString())
+      {
+        case 'works':
+        case 'uploads':
+            console.log(path+' download!');
+            res.download(path);
+            break;
+      }
+    });
+
+    app.delete('/delconvf', function(req, res){
+        var filer = JSON.parse(req.query.file);
+        console.log(filer);
+        var file = filer.convPath;
+        var path = __dirname + '/' + file;
+        var folder1 = file.match(/[^\\\/]+/);
+        //var deletef = JSON.parse(req.body);
+        switch (folder1.toString()) {
+        case 'works':
+        case 'uploads':
+            fs.exists(path,function(exists){  
+                if(exists){  
+                    fs.unlink(path, function(err){
+                        if(err){
+                            console.log(err);
+                            //throw err;
+                        }else{
+                            console.log('file:'+path+' deleted'); 
+                            filer.convFlag = false;
+                            res.send(filer);
+                        }
+                    });
+                }
+            }); 
+            break;
+        }
+        filer.convFlag = false;
+        res.send(filer);
     });
 
     app.listen('3100', function(){
